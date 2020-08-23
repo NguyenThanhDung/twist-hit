c0df3808026a79d8cc8f4618ccb94049ac510e78
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-08-23 10:18:44 +0700

    Assign material

--- Assets/Scripts/DynamicPie.cs
+++ Assets/Scripts/DynamicPie.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class DynamicPie : MonoBehaviour
 {
+    [SerializeField] private Material material;
+
     void Start()
     {
-        gameObject.AddComponent<MeshFilter>();
-        gameObject.AddComponent<MeshRenderer>();
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
 
+        Mesh mesh = meshFilter.mesh;
         mesh.Clear();
-
         mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0) };
         mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
         mesh.triangles = new int[] { 0, 1, 2 };
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material = this.material;
     }
 }
