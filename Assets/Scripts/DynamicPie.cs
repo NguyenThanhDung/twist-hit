a4d550516b4ff452b99c7930f78e5979001e01ba
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-08-23 10:26:50 +0700

    Generate data by functions

--- Assets/Scripts/DynamicPie.cs
+++ Assets/Scripts/DynamicPie.cs
@@ -6,17 +6,44 @@ public class DynamicPie : MonoBehaviour
 {
     [SerializeField] private Material material;
 
-    void Start()
+    private void Start()
     {
         MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
 
         Mesh mesh = meshFilter.mesh;
         mesh.Clear();
-        mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0) };
-        mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
-        mesh.triangles = new int[] { 0, 1, 2 };
+        mesh.vertices = GetVertices();
+        mesh.uv = GetUVs();
+        mesh.triangles = GetTriangles();
 
         MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
         meshRenderer.material = this.material;
+    }
+
+    private Vector3[] GetVertices()
+    {
+        Vector3[] vertices = new Vector3[3];
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(0, 1, 0);
+        vertices[2] = new Vector3(1, 1, 0);
+        return vertices;
+    }
+
+    private Vector2[] GetUVs()
+    {
+        Vector2[] uvs = new Vector2[3];
+        uvs[0] = new Vector2(0, 0);
+        uvs[1] = new Vector2(0, 1);
+        uvs[2] = new Vector2(1, 1);
+        return uvs;
+    }
+
+    private int[] GetTriangles()
+    {
+        int[] triangles = new int[3];
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        return triangles;
     }
 }
