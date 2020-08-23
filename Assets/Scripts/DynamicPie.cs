c28f90a17caab6fd34fcddd75a0fd946adf7ddb2
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-08-23 10:11:45 +0700

    Add first triangle

--- Assets/Scripts/DynamicPie.cs
+++ Assets/Scripts/DynamicPie.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class DynamicPie : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        mesh.Clear();
+
+        mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0) };
+        mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
+        mesh.triangles = new int[] { 0, 1, 2 };
     }
 }
